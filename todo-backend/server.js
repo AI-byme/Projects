329ff477af3ef12fccb6fa0e8705db1fc29581f2
Arifin Ilham <arifinilham371@gmail.com>
2025-06-27 21:59:16 +0700

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -3,6 +3,17 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 const cors = require("cors");
 
+app.use(cors({
+  origin: ["http://localhost:5173", "https://projects-production-0fac.up.railway.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
+// Tangani preflight request
+app.options("*", cors());
+
+
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
