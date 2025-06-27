6915fea625afc2d02488b232b598bd1de9872375
Arifin Ilham <arifinilham371@gmail.com>
2025-06-27 21:59:52 +0700

    Update app.js

--- todo-backend/app.js
+++ todo-backend/app.js
@@ -8,17 +8,6 @@ const cors = require("cors");
 // Middleware parsing JSON
 app.use(express.json());
 
-// Middleware CORS (penting!)
-app.use(cors({
-  origin: ["http://localhost:5173", "https://projects-production-0fac.up.railway.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
-// Tangani preflight request
-app.options("*", cors());
-
 // Logging sederhana
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
